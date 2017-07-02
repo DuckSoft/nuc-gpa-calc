c2c5d99d77fdae632b1536872a698bd3f907d684
DuckSoft <ducksoft@foxmail.com>
2017-07-02 22:57:47 +0800

    gpa.c: 将问号表达式拆成一个内联函数以提高代码可维护性

--- gpa.c
+++ gpa.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 
+inline float getGP(float score, float point) {
+	if (score < 60) {
+		return 0.0;
+	} else {
+		return point*(score/10-5);
+	}
+}
+
+
 int main() {
 	float sumPoint = 0;	//课程学分总数
 	float sumGP = 0.0;	//课程学分绩点总数
-	
-	int n;	//课程编号 
+
+	int n;	//课程编号
 	for (n=1; ; n++) {
-		float nPoint;	//本课程学分 
+		float nPoint;	//本课程学分
 		printf("课程 #%d 学分(如：4.0；输入负数结束):\n ->", n);
 		scanf("%f", &nPoint);
-		
+
 		if (nPoint < 0) {
 			if (n == 1) {
-				//在第一个课程就结束时不必参与后面计算 
+				//在第一个课程就结束时不必参与后面计算
 				//应予以直接结束
-				return 0; 
+				return 0;
 			}
 			break;
 		} else {
-			sumPoint += nPoint; 
-			
-			int nScore;	//本课程考试成绩 
+			sumPoint += nPoint;
+
+			int nScore;	//本课程考试成绩
 			printf("课程 #%d 考试成绩(0～100):\n ->", n);
 			scanf("%d",&nScore);
 
-			sumGP += ((nScore<60)?(0.0):(nPoint*(nScore/10-5)));
+			sumGP += getGP(nScore,nPoint);
 		}
 	}
 	printf("你的GPA是：%.1f\n",sumGP/sumPoint);
